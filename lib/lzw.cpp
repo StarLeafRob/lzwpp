437d9aee1212a123852d9d28ab08507b387d5498
rdt <rob.taylor@starleaf.com>
2017-02-20 20:00:31 +0000

    initial commit

---
+++ lib/lzw.cpp
@@ -0,0 +1,15 @@
+/*
+ *      Author: rdt
+ */
+
+#include "lib/lzw.hpp"
+
+namespace lzw {
+
+bool decompress (const std::string& input, const std::string& output) {
+    return true;
+}
+
+}
+
+
---
+++ lib/lzw.hpp
@@ -0,0 +1,12 @@
+/*
+ *      Author: rdt
+ */
+
+#pragma once
+#include <string>
+
+namespace lzw {
+
+bool decompress (const std::string& input, const std::string& output);
+
+}
